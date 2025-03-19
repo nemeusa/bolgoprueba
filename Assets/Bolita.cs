b3dd065bb64feb0efe9b6ed4c9f0799d73fe5100
nemeusa <nemeusa@gmail.com>
2025-03-19 12:55:45 -0300

    update

--- Assets/Bolita.cs
+++ Assets/Bolita.cs
@@ -7,15 +7,15 @@ public class Bolita : MonoBehaviour
     public float speed;
     Rigidbody rb;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        launch();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
+       rb.velocity = rb.velocity.normalized * speed;
     }
 
     void launch()
@@ -24,5 +24,23 @@ public class Bolita : MonoBehaviour
         float y = Random.Range(-2, 2);
 
         rb.velocity = new Vector3(x, y, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 normal = collision.contacts[0].normal;
+        Vector3 nextDirection = Vector3.Reflect(rb.velocity, normal);
+
+        if(Mathf.Abs(nextDirection.y) < 0.1f)
+        {
+            nextDirection.y = Random.Range(-7, 7);
+        }
+        
+        if(Mathf.Abs(nextDirection.x) < 0.1f)
+        {
+            nextDirection.x = Random.Range(-7, 7);
+        }
+
+        rb.velocity = nextDirection.normalized * speed;
     }
 }
